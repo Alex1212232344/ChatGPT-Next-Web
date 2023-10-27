ec6f987f56a17088ae442454c79f96ea849e26f1
alt4real <147963351+alt4real@users.noreply.github.com>
2023-10-27 15:43:43 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "普通",
+  title: "Agora",
   description: "",
   viewport: {
     width: "device-width",
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "普通",
+    title: "Agora",
     statusBarStyle: "default",
   },
 };
