70c8ad4926dbbf36fd56786f7914f0be7484c346
alt4real <932853219@qq.com>
2023-10-27 22:35:41 +0800

    引流

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -8,7 +8,7 @@ const cn = {
   Error: {
     Unauthorized: isApp
       ? "检测到无效 API Key，请前往[设置](/#/settings)页检查 API Key 是否配置正确。"
-      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码。",
+      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码。或者来看看[别的](https://yetey-1api.hf.space/about)？",
   },
   Auth: {
     Title: "需要密码",
